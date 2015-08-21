cb3f7c64a84d613bfe70101f70717321e429d179
Jared Hyers <jared@laicos.com>
2015-08-21 09:44:19 -0400

    Refactor
    Make changes in app.js for better control flow through catLevel.
    Set up the viewModel to receive multiple cats

--- js/app.js
+++ js/app.js
@@ -1,30 +1,47 @@
 var ViewModel = function(){
-   this.currentCat = ko.observable(new Cat());
+   this.currentCat = ko.observable(new Cat({
+      clickCount : 0,
+      name : "Tabby",
+      imgSrc:"img/434164568_fea0ad4013_z.jpg",
+      nickNames:['Tabs', 'T-Diddy', 'T-No-Show']
+   }));
 
    this.incrementCounter = function(){
       this.clickCount(this.clickCount() + 1);
    };
 }
 
-var Cat = function(){
-   this.clickCount = ko.observable(0);
-   this.name = ko.observable('Tabby');
-   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-   this.catNewborn = ko.observable('Newborn');
-   this.catInfant = ko.observable('Infant');
-   this.catTeen = ko.observable('Teen');
-   this.nickNames = ko.observableArray(['Tabs', 'T-diddy', 'T-No-Show']);
+var Cat = function(data){
+   this.clickCount = ko.observable(data.clickCount);
+   this.name = ko.observable(data.name);
+   this.imgSrc = ko.observable(data.imgSrc);
+   this.nickNames = ko.observableArray(data.nickNames);
 
    this.catLevel = ko.computed(function(){
-      if (this.clickCount() <= 10){
-         return this.catNewborn();
+      var title;
+      var clicks= this.clickCount();
+      if (clicks < 10){
+         title = "Newborn";
       }
-      else if (this.clickCount() <= 19 ) {
-         return this.catInfant();
+      else if (clicks < 20 ) {
+         title = "Infant";
       }
-      else if (this.clickCount() >= 20){
-         return this.catTeen();
+      else if (clicks < 30){
+         title = "Teen";
       }
+      else if (clicks < 50){
+         title = "Young Adult";
+      }
+      else if (clicks < 100){
+         title = "Adult";
+      }
+      else if (clicks < 200){
+         title = "Giezer";
+      }
+      else{
+         title = "Ninja";
+      }
+      return title;
    }, this)
 }
 

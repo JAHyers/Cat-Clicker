0087e27a92fcf382c18d8b8c8b0a93febf0017dd
Jared Hyers <jared@laicos.com>
2015-08-21 10:09:33 -0400

    Full functionality
    Cat Clicker Premium Pro is now operational

--- js/app.js
+++ js/app.js
@@ -1,21 +1,61 @@
+var initialCats = [
+   {
+      clickCount: 0,
+      name: "Tabby",
+      imgSrc: "img/434164568_fea0ad4013_z.jpg",
+      nickNames: 'TNoShow'
+   },
+   {
+      clickCount: 0,
+      name: "Tiger",
+      imgSrc: "img/4154543904_6e2428c421_z.jpg",
+      nickNames: "Tigger"
+   },
+   {
+      clickCount: 0,
+      name: "Scaredy",
+      imgSrc: "img/22252709_010df3379e_z.jpg",
+      nickNames: ""
+   },
+   {
+      clickCount: 0,
+      name: "Shadow",
+      imgSrc: "img/1413379559_412a540d29_z.jpg",
+      nickNames: "Casper"
+   },
+   {
+      clickCount: 0,
+      name: "Sleepy",
+      imgSrc: "img/9648464288_2516b35537_z.jpg",
+      nickNames: "Zzzzz"
+   }
+]
+
 var ViewModel = function(){
-   this.currentCat = ko.observable(new Cat({
-      clickCount : 0,
-      name : "Tabby",
-      imgSrc:"img/434164568_fea0ad4013_z.jpg",
-      nickNames:['Tabs', 'T-Diddy', 'T-No-Show']
-   }));
+   var self = this;
+
+   this.catList = ko.observableArray([]);
+
+   initialCats.forEach(function(catItem){
+      self.catList.push( new Cat(catItem));
+   });
+
+   this.currentCat = ko.observable(this.catList()[0]);
 
    this.incrementCounter = function(){
-      this.clickCount(this.clickCount() + 1);
+      self.currentCat().clickCount(self.currentCat().clickCount() + 1);
    };
-}
+
+   this.setCat = function(clickedCat){
+      self.currentCat(clickedCat);
+   };
+};
 
 var Cat = function(data){
    this.clickCount = ko.observable(data.clickCount);
    this.name = ko.observable(data.name);
    this.imgSrc = ko.observable(data.imgSrc);
-   this.nickNames = ko.observableArray(data.nickNames);
+   this.nickNames = ko.observable(data.nickNames);
 
    this.catLevel = ko.computed(function(){
       var title;
